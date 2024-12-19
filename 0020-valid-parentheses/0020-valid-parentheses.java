bc0279e424e82ef3d4526137f82dfac6f63c396d
TEJAS-SAI-PRASHAD-K <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2024-12-20 02:36:14 +0530

    Time: 4 ms (15.59%), Space: 41.7 MB (63.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean isValid(String s) {
         
         Stack<Character> st = new Stack<Character> ();
-        // String [] opening = {'(','{','['};
-        // String [] closing = {')','}',']'};
         for(int i=0;i < s.length();i++){
             if (s.charAt(i) == '(' | s.charAt(i) == '{' | s.charAt(i) == '['){
                 st.push(s.charAt(i));
