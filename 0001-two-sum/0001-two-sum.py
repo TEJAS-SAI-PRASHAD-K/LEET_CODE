818a067330eeda1a9881aee2ed0d48798b1d76a5
TEJAS-SAI-PRASHAD-K <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2025-07-29 22:54:18 +0530

    Time: 4838 ms (5%), Space: 18.4 MB (78.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,14 @@
-class Solution(object):
-  def twoSum(self, nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        start = 0
+        nxt = start + 1
+        while(start < len(nums)-1):
+            if nxt == len(nums):
+                start += 1
+                nxt = start + 1
+                continue
+            if nums[start]+nums[nxt] == target:
+                return [start,nxt]
+            else:
+                nxt += 1
+        
