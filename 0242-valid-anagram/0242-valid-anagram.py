efecd0ad9f00b0b85958825d009f8bba283391b6
Tejas Sai Prashad <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2025-09-27 21:10:42 +0530

    Time: 7 ms (89.28%), Space: 17.7 MB (86.6%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        hash_table1 = {}
+        hash_table2 = {}
+
+        for i in s:
+            hash_table1[i] = hash_table1.get(i, 0) + 1
+
+        for j in t:
+            hash_table2[j] = hash_table2.get(j, 0) + 1
+
+        for key, value in hash_table1.items():
+            if value != hash_table2.get(key, 0):
+                return False
+
+        return True
