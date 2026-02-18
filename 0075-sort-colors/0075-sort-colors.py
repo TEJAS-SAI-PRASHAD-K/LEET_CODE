774d380c2264e9512efffac558a09ba6d2031793
Tejas Sai Prashad <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2026-02-18 19:48:10 +0530

    Time: 0 ms (100%), Space: 19.5 MB (18.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,19 @@
 class Solution:
-    def quicksort(self,arr,low,high):
-        if low < high:
-            mid = self.partition(arr,low,high)
-            self.quicksort(arr, low, mid - 1)
-            self.quicksort(arr, mid + 1, high)
-
-    def partition(self,arr, low, high):
-        pivot = arr[high]
-        i = low - 1
-        for j in range(low, high):
-            if arr[j] <= pivot:
-                i += 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i+1], arr[high] = arr[high], arr[i+1]
-        return i + 1
-
     def sortColors(self, nums: List[int]) -> None:
-        self.quicksort(nums, 0, len(nums)-1)
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
