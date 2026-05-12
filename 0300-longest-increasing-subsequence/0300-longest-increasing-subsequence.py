aca8bf5ca4f38a8488f3b0588176ce8aa00ac03f
Tejas Sai Prashad <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2026-05-12 20:38:43 +0530

    Time: 1245 ms (51.95%), Space: 19.4 MB (85.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,22 +1,12 @@
 class Solution:
-    def LIS(self,nums: List[int],ind: int,prev: int,dp: List[int][int]) -> int:
-        if ind == len(nums):
-            return 0
-        if dp[ind][prev+1] != -1:
-            return dp[ind][prev+1]
-
-        pick = 0
-        if prev == -1 or nums[ind] > nums[prev]:
-            pick = 1 + self.LIS(nums,ind+1,ind,dp)
-
-        not_picked = self.LIS(nums,ind+1,prev,dp)
-
-        dp[ind][prev+1] = max(pick,not_picked)
-
-        return dp[ind][prev+1]
-
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS(self, nums):
         n = len(nums)
-        dp = []
-        dp = [[-1 for _ in range(n + 1)] for _ in range(n)]
-        return self.LIS(nums,0,-1,dp)
+
+        dp = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
